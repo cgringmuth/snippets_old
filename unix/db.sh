5b36f0f944a45ba248a9da0084c52c933f51abdf
Christoph Hoffmann <choffmann1983@gmail.com>
2014-02-04 09:01:16 +0100

    modified *nix (shell) * added find snippet to exclude folder

--- unix/db.sh
+++ unix/db.sh
@@ -2,3 +2,8 @@
 
 # Find: How to avoid printing to screen Permission denied errors
 find / -name art  2>&1 | grep -v "Permission denied"
+# or pipe stderr to null
+find / -name art  2>/dev/null
+
+# find libglut* in all folders but /home
+find / -name "libglut*" -not -path "/home/*" 2>/dev/null
